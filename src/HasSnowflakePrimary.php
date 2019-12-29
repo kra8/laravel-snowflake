38dabe41a7ce0a96f582c7e8caa9dd1cae5e5273
Jess Carlos <jess@ignittion.com>
2019-12-28 19:16:15 -0800

    automatically set  to false when saving.

--- src/HasSnowflakePrimary.php
+++ src/HasSnowflakePrimary.php
@@ -9,6 +9,7 @@ trait HasSnowflakePrimary
     {
         static::saving(function ($model) {
             if (is_null($model->getKey())) {
+                $model->setIncrementing(false);
                 $keyName    = $model->getKeyName();
                 $id         = app(Snowflake::class)->next();
                 $model->setAttribute($keyName, $id);
